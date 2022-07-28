4cd1f30fae1ba53f35a51e65e69caa63b1a28447
cteyr123 <cteyr123@gmail.com>
2022-07-28 13:46:47 +0200

    typo url fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetupsPage";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetup";
-import {
-  ALL_MEETUP_PAGE,
-  FAVORITES_PAGE,
-  NEW_MEETUP_PAGE,
-} from "./utils/constants";
-
 import MainNavigation from "./components/layout/MainNavigation";
 import Layout from "./components/layout/Layout";
 
 function App() {
-  const [page, setPage] = useState(ALL_MEETUP_PAGE);
-
   return (
     <div data-test="app">
-      <MainNavigation setPage={setPage} />
+      <MainNavigation setPage={<AllMeetupsPage />} />
       <Layout>
         <BrowserRouter>
           <Routes>
