e51f4fed1c60afdca151f1901a23565c890f1b0c
Carlos <ctey@divisait.com>
2022-12-27 20:50:08 +0100

    navBar estilos add

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -3,10 +3,31 @@ import classes from "./MainNavigation.module.css";
 import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 import { useMeetupsContext } from "../../context/MeetupsProvider";
+import { useState } from "react";
 
 export default function MainNavigation({ setPage }) {
   const { isScrollingUp, isScrollingDown } = useScroll();
   const { favoritesCount } = useMeetupsContext();
+  const [isActivate, setActivate] = useState(true);
+
+  window.addEventListener("resize", function(){
+    const pageWidth  = document.documentElement.scrollWidth;
+    if(pageWidth<=468){
+      document.getElementById("listNav").style.display = "none";
+    }else{
+      document.getElementById("listNav").style.display = "flex";
+    }
+  });
+
+  const onClickHamburguer =() =>   {
+    if(isActivate===false){
+      setActivate(true);
+      document.getElementById("listNav").style.display = "none";
+    }else{
+      setActivate(false);
+      document.getElementById("listNav").style.display = "flex";
+    }
+  };
 
   return (
     <header
@@ -17,9 +38,9 @@ export default function MainNavigation({ setPage }) {
       data-test="navigation-header"
     >
       <div className={classes.logo}>React Meetups</div>
-      <div className={classes.hamburguer}></div>
+      <div className={classes.hamburguer} onClick={onClickHamburguer}></div>
       <nav>
-        <ul>
+        <ul id="listNav">
           <li>
             <NavLink to="meetups">All Meetups</NavLink>
           </li>
